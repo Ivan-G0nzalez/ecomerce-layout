c134e4ce466723c3634de529ab1c88b81db80b0a
Ivan_G0nzalez <iegdlt@gmail.com>
2025-06-16 15:45:40 -0500

    Social section added

---
+++ src/components/SocialHeader/SocialSection.tsx
@@ -0,0 +1,12 @@
+import { FaInstagram } from "react-icons/fa";
+
+function SocialSection() {
+  return (
+    <div className='flex items-center justify-center text-3xl font-libre gap-2 mt-12'>
+      <FaInstagram />
+      <span>Instagram - HUSHPUPPIESCO</span>
+    </div>
+  );
+}
+
+export default SocialSection;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,20 @@
-import CategoriesGrid from "~/components/Categories/CategoriesGrid";
-import HeroBannerMove from "~/components/MainSection/HeroBanner";
-import HeroSubSection from "~/components/MainSection/HeroSubSection";
-import VideosSection from "~/components/MainSection/VideosSecetion";
+
+import CategoriesGrid from '~/components/Categories/CategoriesGrid';
+import HeroBannerMove from '~/components/MainSection/HeroBanner';
+import HeroSubSection from '~/components/MainSection/HeroSubSection';
+import VideosSection from '~/components/MainSection/VideosSecetion';
+import SocialSection from '~/components/SocialHeader/SocialSection';
 
 const Home = () => {
-    return (<>
-        <HeroBannerMove />
-        <CategoriesGrid />
-        <VideosSection />
-        <HeroSubSection />
-    </>);
-}
+  return (
+    <>
+      <HeroBannerMove />
+      <CategoriesGrid />
+      <VideosSection />
+      <HeroSubSection />
+      <SocialSection />
+    </>
+  );
+};
 
 export default Home;
