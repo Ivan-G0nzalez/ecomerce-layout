a848fb4f6a3994d50ff9065f76389bbb36affa0d
Ivan_G0nzalez <iegdlt@gmail.com>
2025-06-16 21:32:19 -0500

    Fix 3th section

--- src/components/MainSection/VideosSecetion.tsx
+++ src/components/MainSection/VideosSecetion.tsx
@@ -7,16 +7,21 @@ const VideosSection = () => {
           alt="Hero model"
           className="absolute top-3/5 -translate-y-1/2 left-2/12 bottom-0 md:h-[120%] lg:h-[180%] object-contain z-10"
         />
-        <div className="absolute right-16 lg:right-62 top-32 font-bold text-2xl text-orange-500 tracking-wider rotate-[-90deg]">
+        {/* <div className="absolute right-16 lg:right-62 top-32 font-bold text-2xl text-orange-500 tracking-wider rotate-[-90deg]">
           Hush Puppies ®
-        </div>
+        </div> */}
+        <img
+          src="/BlueShoes2.png"
+          alt="Hero model"
+          className="absolute top-3/6 -translate-y-1/2 left-7/12 bottom-0 md:h-[120%] lg:h-[95%] object-contain z-10"
+        />
   
         {/* Botón */}
-        <div className="absolute bottom-8 right-82 z-20 lg:right-135">
+        {/* <div className="absolute bottom-8 right-82 z-20 lg:right-135">
           <button className="text-[#b5a59e] px-8 py-1 bg-white hover:bg-gray-100 transition text-base font-semibold shadow-[0px_4px_10px_6px_rgba(0,0,0,0.15)]">
             SHOP NOW
           </button>
-        </div>
+        </div> */}
       </section>
     )
 }
