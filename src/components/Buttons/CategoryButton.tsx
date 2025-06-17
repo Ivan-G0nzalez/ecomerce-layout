4f6d1e44f77cdc288c1ac28c374a5240aad46f52
Ivan_G0nzalez <iegdlt@gmail.com>
2025-06-16 21:21:06 -0500

    Fix the sise of the buttton in a big screen

--- src/components/Buttons/CategoryButton.tsx
+++ src/components/Buttons/CategoryButton.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const CategoryButtons:FC<IProps> = ({label, root, invertColor}) => {
     return (
         <a
-        className={`absolute bottom-8 left-1/2 -translate-x-1/2 px-8 py-2 text-base font-semibold 
+        className={`absolute bottom-8 left-1/2 -translate-x-1/2 px-8 2xl:px-14 2xl:py-6 2xl:text-2xl py-2 text-base font-semibold 
             group-hover:bg-black transition-all duration-300 ease-in-out ${!invertColor ? 'bg-[#595558] text-white': 'bg-white text-[#595558]'}`}
       >
         {label}
