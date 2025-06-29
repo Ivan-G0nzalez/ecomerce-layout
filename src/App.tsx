d59c4b35c7d03a22833f1136752d854009cf1586
Ivan_G0nzalez <iegdlt@gmail.com>
2025-06-29 11:38:16 -0500

    Remove unused product components and images, rename CategorPages to CategoryPage, and update routing in App.tsx for improved clarity and organization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home'
 import Layout from './Layouts/Layout';
-import CategorPages from './pages/Categories/CategoriesPage';
+import CategoryPage from './pages/Categories/CategoriesPage';
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="categorias" element={<CategorPages />} />
+        <Route path="categorias" element={<CategoryPage />} />
       </Route>
     </Routes>
   )
--- src/pages/Categories/CategoriesPage.tsx
+++ src/pages/Categories/CategoriesPage.tsx
@@ -1,9 +1,69 @@
-import Products from "~/components/Products/Products";
+import { useEffect, useState } from 'react';
+import ProductsGrid from '~/components/ProductsCategories/ProductsGrid';
+import Sidebar from '~/components/ProductsCategories/SideBar';
+import { useAppDispatch } from '~/hooks/useShoesSelector'; 
+import { loadShoes } from '~/store/shoes';
+import { useShoesSelector } from '~/hooks/useShoesSelector';
 
-const CategorPages = () => {
-    return (
-        <Products />
-    )
-} 
 
-export default CategorPages;
+const CategoryPage = () => {
+  const dispatch = useAppDispatch();
+  const { shoes:products } = useShoesSelector();
+
+  useEffect(() => {
+    dispatch(loadShoes());
+  }, []);
+
+  const [selectedSizes, setSelectedSizes] = useState<string[]>([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+
+  const sizes = ['5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5'];
+  const colors = ['NEGRO(12)', 'AZUL(12)', 'CAFÉ(12)', 'ORO(12)'];
+  const materials = ['Cuero(18)', 'Nubuck(8)'];
+  const technologies = ['HD7/Flex(5)', 'BioBewell(1)', 'HexGroove(3)'];
+
+
+  const toggleFavorite = (productId: number) => {
+    setFavorites((prev) =>
+      prev.includes(productId)
+        ? prev.filter((id) => id !== productId)
+        : [...prev, productId]
+    );
+  };
+
+  const toggleSelection = (
+    item: string,
+    _selectedItems: string[],
+    setSelectedItems: React.Dispatch<React.SetStateAction<string[]>>
+  ) => {
+    setSelectedItems((prev) =>
+      prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]
+    );
+  };
+  return (
+    <div className=''>
+      <div className='2xl:px-32 px-16 py-8'>
+        <div className='flex gap-8'>
+          <Sidebar
+            sizes={sizes}
+            selectedSizes={selectedSizes}
+            materials={materials}
+            colors={colors}
+            technologies={technologies}
+            onSizeToggle={(size) =>
+              toggleSelection(size, selectedSizes, setSelectedSizes)
+            }
+          />
+
+          <ProductsGrid
+            products={products}
+            favorites={favorites}
+            onToggleFavorite={toggleFavorite}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
